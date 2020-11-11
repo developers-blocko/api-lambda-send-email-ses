2ad3de3131f09a85d805aaf4f5dfff3f963d7eee
Jungsoo Yang <jungsoo827@gmail.com>
2020-11-11 12:54:54 +0900

    updated to send mesasges to slack channel

--- index.js
+++ index.js
@@ -1,63 +1,32 @@
 const AWS = require('aws-sdk'),
-  SES = new AWS.SES(),
-  processResponse = require('./process-response.js'),
-  FROM_EMAIL = process.env.FROM_EMAIL,
-  UTF8CHARSET = 'UTF-8';
+    SES = new AWS.SES(),
+    processResponse = require('./process-response.js'),
+    UTF8CHARSET = 'UTF-8';
+const axios = require('axios');
+
+const slackUrl = "https://hooks.slack.com/services/sdfsdfsdf";
 
 exports.handler = async event => {
   if (event.httpMethod === 'OPTIONS') {
     return processResponse(true);
   }
 
-  if (!event.body) {
-    return processResponse(true, 'Please specify email parameters: toEmails, subject, and message ', 400);
-  }
-  const emailData = JSON.parse(event.body);
-
-  if (!emailData.toEmails || !Array.isArray(emailData.toEmails) || !emailData.subject || !emailData.message) {
-    return processResponse(true, 'Please specify email parameters: toEmails, subject and message', 400);
-  }
-
-  const destination = {
-    ToAddresses: emailData.toEmails
-  }
-
-  if (emailData.ccEmails) {
-    destination.CcAddresses = emailData.ccEmails;
-  }
-
-  const body = (emailData.message && isHTML(emailData.message)) ?
-    { Html: { Charset: UTF8CHARSET, Data: emailData.message } } :
-    { Text: { Charset: UTF8CHARSET, Data: emailData.message } };
-
-  const emailParams = {
-    Destination: destination,
-    Message: {
-      Body: body,
-      Subject: {
-        Charset: UTF8CHARSET,
-        Data: emailData.subject
-      }
-    },
-    Source: FROM_EMAIL
-  };
-
-  if (emailData.replyToEmails && Array.isArray(emailData.replyToEmails)) {
-    emailParams.ReplyToAddresses = emailData.replyToEmails;
-  }
+  // console.log(event);
+  const eventString = JSON.stringify(event);
 
   try {
-    await SES.sendEmail(emailParams).promise();
+    const slackMessage = {
+      text: "Queue Trigger - " + eventString
+    };
+    console.log(slackMessage);
+    const res = await axios.post(slackUrl, slackMessage);
+    console.log(`res.data: ${res.data}`)
     return processResponse(true);
-  } catch (err) {
+
+  } catch (err){
     console.error(err, err.stack);
-    const errorResponse = `Error: Execution update, caused a SES error, please look at your logs.`;
+    const errorResponse = `Error: Execution update, caused a slack post error, please look at your logs.`;
     return processResponse(true, errorResponse, 500);
   }
 };
 
-function isHTML(value) {
-  value = value.trim();
-  return value.startsWith('<') && value.endsWith('>') &&
-    (value.includes('<body') || value.includes('<div') || value.includes('<s') || value.includes('<h') || value.includes('<p'));
-}
